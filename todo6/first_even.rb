ab5150e4c335d41192ef15a1c55e9a99a026b5b2
hannah nordgren <hannah.nordgren@flatironschool.com>
2013-11-19 14:33:12 -0500

    added birthdays thing

---
+++ todo6/first_even.rb
@@ -0,0 +1,47 @@
+class Even
+
+array = [30, 17, 77, 102]
+
+def first_even(array)
+  puts array[0]
+end
+
+array
+
+end
+
+#Gonna come back to this idea later.
+# numbers = [30, 17, 77, 102]
+
+# def first_even(numbers)
+# 	case even_first
+# 	when condition numbers[0].even?
+# 		puts numbers[0]
+# 	when numbers[0] == 30
+# 		numbers << 5
+# 		numbers << 11
+# 		numbers << 50
+# 	end
+
+	# numbers.reverse!
+	# puts numbers[0]
+
+	# case even_2
+	# when numbers[0].even?
+	# 	 puts numbers[0]
+	# when numbers[0] == 50
+	# 	numbers << 3
+	# 	numbers << 5
+	# 	numbers << 15
+	# 	numbers << 23
+	# 	numbers << 42
+	# 	numbers << 16
+	# end
+	
+	# numbers.reverse!
+
+	# case even_3
+
+# end
+
+# first_even
