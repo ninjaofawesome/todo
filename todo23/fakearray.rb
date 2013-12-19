e4b3f219eed11e270facd786b7fc2a98506dce53
hannah nordgren <hannah.nordgren@flatironschool.com>
2013-12-19 10:48:29 -0500

    covered in today's lecture

---
+++ todo23/fakearray.rb
@@ -0,0 +1,59 @@
+# To explore the confusion around the results object we get back from queries using the mysql2 gem...
+
+# Create an object that behaves like an array but is NOT an array. Define the each, first and indexing methods on it.
+
+# I should get the same result if i do
+
+# container = []
+# [1,2,3].each do |element|
+#   container << element + 1
+# end
+
+# as if i use your class like so...
+
+# fakearray = FakeArray.new
+# container = []
+# fakearray.each do |element|
+#   container << element + 1
+# end
+
+# Note: I've intentionally omitted a step here where you'd have to somehow tell your class that were using an array of 1,2,3
+
+# class FakeArray
+
+#   def each
+    
+#   end
+
+#   def first
+    
+#   end
+
+#   def []()
+    
+#   end
+
+# end
+
+
+class FakeArray
+
+	def each
+		not_an_array = {:number => 1, :number => 2, :number => 3 }
+		@container = []
+			not_an_array.each do |value|
+				@container << value
+			end
+		@container
+	end
+
+	def first
+
+	end
+
+end
+
+fakearray = FakeArray.new
+p fakearray.each
+
+
